77c95713d6064e19fab197a58d783ded9fc99b73
Duy Phạm <duypham@Duys-MacBook-Pro.local>
2019-01-24 19:04:40 +0700

    updated create account

--- server/src/main/java/com/shopping/util/Test.java
+++ server/src/main/java/com/shopping/util/Test.java
@@ -1,7 +1,8 @@
 package com.shopping.util;
 
 public class Test {
-	private static final int[] dayAndYear = { 0, 1900, 2, 2000, 1 };
+	private static final int[] dayAndYear = { 0, 1500, 2, 1600, 1, 1700, 6, 1800, 4, 1900, 2, 2000, 1, 2100, 6, 2200, 4,
+			2300, 2, 2400, 1, 2500, 6 };
 
 	public static boolean isLeapYear(int year) {
 		int twoLastDigitsOfYear = year % 100;
@@ -25,29 +26,13 @@ public class Test {
 		int i;
 		int dateOfYear = 0;
 
-		if (!isLeapYear(year)) {
-			System.out.println("first");
-			temp1 = lastTwoDigits / 12;
-			temp2 = lastTwoDigits % 12;
-			temp3 = temp2 / 4;
-			temp4 = (temp1 + temp2 + temp3) % 7;
-			for (i = 1; i < dayAndYear.length - 1; i += 2) {
-
-				if (dayAndYear[i] / 100 == firstTwoDigits) {
-					dayAndYear[i + 1] += temp4;
-					dateOfYear = dayAndYear[i + 1];
-				}
-			}
-			return dateOfYear;
-		}
-		System.out.println("second");
 		temp1 = lastTwoDigits / 12;
 		temp2 = lastTwoDigits % 12;
 		temp3 = temp2 / 4;
 		temp4 = (temp1 + temp2 + temp3) % 7;
 		for (i = 1; i < dayAndYear.length - 1; i += 2) {
 
-			if (dayAndYear[i] / 100 == firstTwoDigits) {
+			if ((dayAndYear[i] / 100) == firstTwoDigits) {
 				dayAndYear[i + 1] += temp4;
 				dateOfYear = dayAndYear[i + 1];
 			}
@@ -59,29 +44,45 @@ public class Test {
 	public static void main(String[] args) {
 
 		// leave empty so that months[1] = "January"
-//		String[] month = { "", "January", "February", "March", "April", "May", "June", "July", "August", "September",
-//				"October", "November", "December" };
+		String[] month = { "", "January", "February", "March", "April", "May", "June", "July", "August", "September",
+				"October", "November", "December" };
 
 		int[] day = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-		int yearChoose = 2001;
+		/************* Input here ***************/
+
+		// Please enter the year you want
+		int yearChoose = 2235;
 
 		day[2] = dayOfFeb(yearChoose);
+
+		// Please enter the month you want
+		int monthChoose = 1;
+
+		/************* Input here ***************/
 
 //		int[] date = { 0, 1, 2, 3, 4, 5, 6 };
 		String[] letterDate = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
-		int monthChoose = 4;
-
 //		System.out.println(letterDate[doomsdayOfYear(yearChoose)]);
+		
+		int dayBegin = 0;
 
 		switch (monthChoose) {
 		case 1: {
 			if (!isLeapYear(yearChoose)) {
-				System.out.println(letterDate[(doomsdayOfYear(yearChoose) - 2) % 7]);
+				dayBegin = doomsdayOfYear(yearChoose) - 2;
+				while (dayBegin < 0) {
+					dayBegin = dayBegin + 7;
+				}
+				System.out.println(letterDate[dayBegin]);
 				break;
 			}
-			System.out.println(letterDate[(doomsdayOfYear(yearChoose) - 3) % 7]);
+			dayBegin = doomsdayOfYear(yearChoose) - 3;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
 			break;
 		}
 		case 2: {
@@ -90,18 +91,112 @@ public class Test {
 				break;
 			}
 			System.out.println(letterDate[doomsdayOfYear(yearChoose)]);
+			break;
 		}
 
 		case 3: {
-			System.out.println(letterDate[(doomsdayOfYear(yearChoose) + 1) % 7]);
+			dayBegin = doomsdayOfYear(yearChoose) - 6;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
 			break;
 		}
 
 		case 4: {
-			System.out.println((doomsdayOfYear(yearChoose)));
-			System.out.println(letterDate[(Math.abs(doomsdayOfYear(yearChoose) - 6)) % 7]);
+			dayBegin = doomsdayOfYear(yearChoose) - 3;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
 			break;
 		}
+
+		case 5: {
+			dayBegin = doomsdayOfYear(yearChoose) - 8;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 6: {
+			dayBegin = doomsdayOfYear(yearChoose) - 5;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 7: {
+			dayBegin = doomsdayOfYear(yearChoose) - 10;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 8: {
+			dayBegin = doomsdayOfYear(yearChoose) - 7;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 9: {
+			dayBegin = doomsdayOfYear(yearChoose) - 4;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 10: {
+			dayBegin = doomsdayOfYear(yearChoose) - 9;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 11: {
+			dayBegin = doomsdayOfYear(yearChoose) - 6;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+
+		case 12: {
+			dayBegin = doomsdayOfYear(yearChoose) - 11;
+			while (dayBegin < 0) {
+				dayBegin = dayBegin + 7;
+			}
+			System.out.println(letterDate[dayBegin]);
+			break;
+		}
+		}
+		System.out.println();
+		System.out.println(dayBegin);
+		System.out.println(month[monthChoose]+"  "+yearChoose);
+		System.out.println(" M Tu  W Th  F  S  S");
+		int i;
+		for(i=0; i<dayBegin; i++) {
+			System.out.print("   ");
+		}
+		for(i=1; i <= day[monthChoose]; i++) {
+			System.out.printf("%2d ", i);
+			if(((i + dayBegin) % 7 == 0) || (i == day[monthChoose])) {
+				System.out.println();
+			}
 		}
 
 	}
